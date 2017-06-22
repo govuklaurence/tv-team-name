eb6ba5a2c3f48006841047667702feb1dfc71177
Laurence <laurence.smith@digital.cabinet-office.gov.uk>
2017-06-22 11:30:08 +0100

    Add show routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'create', to: 'team_name#create'
-  get 'show',   to: 'team_name#show'
+  get 'show/:colour/:team_name', to: 'team_name#show'
+  get 'show/:team_name', to: 'team_name#show'
 
   root to: 'team_name#create'
 end

4e91fa262603a9349c81cade435522a7f9fef334
Laurence <laurence.smith@digital.cabinet-office.gov.uk>
2017-06-22 11:27:50 +0100

    Change routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'team_name/create'
-  get 'team_name/show'
+  get 'create', to: 'team_name#create'
+  get 'show',   to: 'team_name#show'
 
   root to: 'team_name#create'
 end

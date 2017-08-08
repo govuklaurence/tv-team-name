7a97f49bb8a14fb009aa3b9ea6793ba9c0e1064f
Alex Torrance <hello@alextorrance.co.uk>
2017-08-08 09:44:24 +0100

    If a background colour is not selected choose one at random

--- app/controllers/team_name_controller.rb
+++ app/controllers/team_name_controller.rb
@@ -7,11 +7,14 @@ class TeamNameController < ApplicationController
   def create_sign
     sign_params = params.permit(:team_name, :colour)
     team_name = sign_params[:team_name]
-    colour = sign_params[:colour]
-    uri_team_name = URI::encode(sign_params[:team_name]).gsub('_', '__').gsub('.', '_')
-    uri_colour = URI::encode(sign_params[:colour])
 
-    if !team_name.strip.empty? && !colour.strip.empty?
+    uri_team_name = URI::encode(sign_params[:team_name]).gsub('_', '__').gsub('.', '_')
+    if !sign_params[:colour].nil?
+      colour = sign_params[:colour]
+      uri_colour = URI::encode(sign_params[:colour].strip)
+    end
+
+    if !team_name.strip.empty? && !colour.nil?
       redirect_to "/sign/#{uri_team_name}/#{uri_colour}"
     elsif !team_name.strip.empty?
       redirect_to "/sign/#{uri_team_name}"
@@ -21,10 +24,15 @@ class TeamNameController < ApplicationController
   end
 
   def sign
-    @colour = params[:colour]
+    if params[:colour].nil?
+      @colour = @colour_palette.sample
+    else
+      @colour = params[:colour]
+    end
     @team_name = params[:team_name].gsub('_', '.').gsub('__', '_')
     @page_title = "#{params[:team_name].gsub('_', '.').gsub('__', '_')} – "
   end
+
   before_action :set_colours
   def set_colours
     @colour_palette = [
